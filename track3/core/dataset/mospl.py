99150dfa7c2d30ba576ff291a234349c592ecda5
k-washi <bandad.kw@gmail.com>
2025-04-21 14:06:44 +0900

    add wav2vecv2 model

--- track3/core/dataset/mospl.py
+++ track3/core/dataset/mospl.py
@@ -6,31 +6,34 @@ from torch.utils.data import DataLoader
 from track3.core.config import Config
 from track3.core.dataset.mosdataset import MOSDataset
 
+
 class MOSDataModule(LightningDataModule):
-    """
-    Data module for the MOS dataset.
-    
+    """Data module for the MOS dataset.
+
     Args:
+    ----
         config (Config): Configuration object containing dataset parameters.
         train_csv (str): Path to the training CSV file.
         val_csv (str): Path to the validation CSV file.
         test_csv (str): Path to the test CSV file.
         wav_dir (str): Path to the directory containing WAV files.
+
     """
-    
+
     def __init__(
-        self, 
+        self,
         config: Config,
-        train_audio_list: list[str|Path],
+        train_audio_list: list[str | Path],
         train_label_list: list[int],
         train_user_id_list: list[int],
-        val_audio_list: list[str|Path],
+        val_audio_list: list[str | Path],
         val_label_list: list[int],
         val_user_id_list: list[int],
-        test_audio_list: list[str|Path],
+        test_audio_list: list[str | Path],
         test_label_list: list[int],
         test_user_id_list: list[int],
     ) -> None:
+        """Initialize the MOSDataModule."""
         super().__init__()
         self.c = config
         self.train_audio_list = train_audio_list
@@ -42,13 +45,14 @@ class MOSDataModule(LightningDataModule):
         self.test_audio_list = test_audio_list
         self.test_label_list = test_label_list
         self.test_user_id_list = test_user_id_list
-    
-    def setup(self, stage: str|None = None) -> None:
-        """
-        Setup the dataset for training, validation, and testing.
-        
+
+    def setup(self, stage: str | None = None) -> None:
+        """Set the dataset for training, validation, and testing.
+
         Args:
+        ----
             stage (str, optional): Stage of the data module. Can be 'fit', 'validate', or 'test'.
+
         """
         if stage == "fit" or stage is None:
             self.train_dataset = MOSDataset(
@@ -56,14 +60,14 @@ class MOSDataModule(LightningDataModule):
                 audio_list=self.train_audio_list,
                 label_list=self.train_label_list,
                 user_id_list=self.train_user_id_list,
-                is_transform=True
+                is_transform=True,
             )
             self.val_dataset = MOSDataset(
                 config=self.c,
                 audio_list=self.val_audio_list,
                 label_list=self.val_label_list,
                 user_id_list=self.val_user_id_list,
-                is_transform=False
+                is_transform=False,
             )
         if stage == "test" or stage is None:
             self.test_dataset = MOSDataset(
@@ -71,14 +75,16 @@ class MOSDataModule(LightningDataModule):
                 audio_list=self.test_audio_list,
                 label_list=self.test_label_list,
                 user_id_list=self.test_user_id_list,
-                is_transform=False
+                is_transform=False,
             )
+
     def train_dataloader(self) -> DataLoader:
-        """
-        Create the training DataLoader.
-        
-        Returns:
+        """Create the training DataLoader.
+
+        Returns
+        -------
             DataLoader: DataLoader for the training dataset.
+
         """
         return DataLoader(
             self.train_dataset,
@@ -89,11 +95,14 @@ class MOSDataModule(LightningDataModule):
             collate_fn=self.train_dataset.collate_fn,
             drop_last=True,
         )
+
     def val_dataloader(self) -> DataLoader:
-        """
-        Create the validation DataLoader.
-        Returns:
+        """Create the validation DataLoader.
+
+        Returns
+        -------
             DataLoader: DataLoader for the validation dataset.
+
         """
         return DataLoader(
             self.val_dataset,
@@ -102,4 +111,21 @@ class MOSDataModule(LightningDataModule):
             num_workers=self.c.ml.num_workers,
             pin_memory=True,
             collate_fn=self.val_dataset.collate_fn,
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        """Create the test DataLoader.
+
+        Returns
+        -------
+            DataLoader: DataLoader for the test dataset.
+
+        """
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.c.ml.test_batch_size,
+            shuffle=False,
+            num_workers=self.c.ml.num_workers,
+            pin_memory=True,
+            collate_fn=self.test_dataset.collate_fn,
         )
---
+++ track3/models/ssl/wav2vec_linear.py
@@ -0,0 +1,58 @@
+import torch
+from torch import nn
+from transformers import Wav2Vec2Model  # pyright: ignore[reportPrivateImportUsage]
+
+
+class MOSPredictorW2V(nn.Module):
+    """A PyTorch model for predicting Mean Opinion Score (MOS) using Wav2Vec2."""
+
+    def __init__(
+        self, ssl_out_dim: int = 768, dropout: float = 0.1, pretrained_model_name: str = "facebook/wav2vec2-base-960h"
+    ) -> None:
+        """Initialize the MOSPredictorW2V model."""
+        super().__init__()
+
+        self.ssl_model = Wav2Vec2Model.from_pretrained(pretrained_model_name)
+        self.ssl_out_dim = ssl_out_dim
+        self.dense = nn.Linear(in_features=self.ssl_out_dim, out_features=self.ssl_out_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.acticvation = nn.Tanh()
+
+        self.output_layer = nn.Linear(in_features=self.ssl_out_dim, out_features=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the model.
+
+        Args:
+        ----
+            x (torch.Tensor): Input tensor of shape (batch_size, seq_len).
+
+        Returns:
+        -------
+            torch.Tensor: Output tensor of shape (batch_size, 1).
+
+        """
+        # Extract features using Wav2Vec2 # ssl_out_dim = 768 (wav2vec2-base-960h)
+        ssl_out = self.ssl_model(x).last_hidden_state  # (batch_size, seq_len, ssl_out_dim)
+        # Take the mean of the features across the time dimension
+        ssl_out = ssl_out.mean(dim=1)  # (batch_size, ssl_out_dim)
+        # Pass through the output layer
+
+        ssl_out = self.dense(ssl_out)  # (batch_size, ssl_out_dim)
+        ssl_out = self.dropout(ssl_out)
+        ssl_out = self.acticvation(ssl_out)
+        # Pass through the output layer
+        out = self.output_layer(ssl_out)  # (batch_size, 1)
+
+        return out
+
+    def freaze_ssl(self) -> None:
+        """Freeze the SSL model parameters."""
+        self.ssl_model.feature_extractor._freeze_parameters()  # noqa: SLF001
+
+
+if __name__ == "__main__":
+    # Example usage
+    model = MOSPredictorW2V()
+    x = torch.randn(2, 320 * 6 + 159)  # Batch of 32 audio samples, each 1 second long
+    output = model(x)
