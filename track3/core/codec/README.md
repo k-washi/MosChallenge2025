73801393ee4ddbb65f1ea0ffd0efb5f6685c3ce5
k-washi <bandad.kw@gmail.com>
2025-04-23 11:26:27 +0900

    audio codec

--- track3/core/codec/README.md
+++ track3/core/codec/README.md
@@ -33,8 +33,26 @@ x68は, codecの一部にランダムなノイズ + (codecの一部を除去 or 
 68G     /data/mosranking/
 ```
 
+flac 16khzに変換する
+
+```bash
+python ./track3/core/codec/to_flac16k.py 
+```
+
+```txt
+2.0G    /data/mosranking/somos/199
+34M     /data/mosranking/track3/199
+112M    /data/mosranking/bvccodd/199
+13G     /data/mosranking/libritts/199
+13G     /data/mosranking/libritts/299
+520M    /data/mosranking/bvccmain/199
+29G     /data/mosranking/
+```
+
 
 # [efficient-speech-codec](https://github.com/yzGuu830/efficient-speech-codec)
+
+## 依存関係のインストール
 
 ```bash
 git clone https://github.com/yzGuu830/efficient-speech-codec.git ./libs/efficient-speech-codec
@@ -43,3 +61,15 @@ mkdir pretrained
 gdown https://drive.google.com/uc?id=180Q4zctqeNnDmRvoMsVQ-3iCB5FriJbN -O ./pretrained/
 unzip ./pretrained/esc_large_non_adv.zip -d ./pretrained/
 ```
+
+```bash
+echo "export PYTHONPATH="$PWD/libs/efficient-speech-codec:$PYTHONPATH"" >>  ~/.bashrc
+
+source ~/.bashrc
+```
+
+## EncDec戦略
+
+num_strems = 6 or 5でx89を作成
+num_strems = 2でx88を作成
+num_strems = 1でx87を作成
---
+++ track3/core/codec/esccodec/model.py
@@ -0,0 +1,132 @@
+from pathlib import Path
+
+import torch
+import yaml
+from esc import ESC  # pyright: ignore[reportMissingImports]
+
+
+class ESCodec:
+    """ESCodec class for encoding and decoding audio using the ESC model."""
+
+    def __init__(
+        self,
+        config_pth: str,
+        model_pth: str,
+    ) -> None:
+        """Initialize the ESCodec with the given configuration and model paths.
+
+        Args:
+        ----
+            config_pth (str): Path to the configuration file.
+            model_pth (str): Path to the model file.
+
+        """
+        self.config_pth = config_pth
+        self.model_pth = model_pth
+        with Path(config_pth).open("r") as f:
+            config = yaml.safe_load(f)
+        state_dict = torch.load(model_pth, map_location="cpu")["model_state_dict"]
+        self.model = ESC(**config["model"])
+        self.model.load_state_dict(state_dict)
+
+    def encode(self, audio: torch.Tensor, num_streams: int = 6) -> tuple[torch.Tensor, torch.Tensor]:
+        """Encode the input audio using the ESCodec model.
+
+        Args:
+        ----
+            audio (torch.Tensor): Input audio tensor.
+            num_streams (int): Number of streams to use for encoding (num_streams*1.5kbps).
+                Defaults to 6.
+
+        Returns:
+        -------
+            torch.Tensor: Encoded audio tensor.
+
+        """
+        with torch.no_grad():
+            codes, f_shape = self.model.encode(audio, num_streams)
+        return codes, f_shape
+
+    def decode(self, encoded_audio: torch.Tensor, f_shape: torch.Tensor) -> torch.Tensor:
+        """Decode the encoded audio using the ESCodec model.
+
+        Args:
+        ----
+            encoded_audio (torch.Tensor): Encoded audio tensor.
+            f_shape (torch.Tensor): Shape of the encoded audio tensor.
+
+        Returns:
+        -------
+            torch.Tensor: Decoded audio tensor.
+
+        """
+        with torch.no_grad():
+            decoded_audio = self.model.decode(encoded_audio, f_shape)
+        return decoded_audio
+
+    def encdec(self, audio: torch.Tensor, num_streams: int = 6) -> tuple[torch.Tensor, torch.Tensor]:
+        """Encode and decode the input audio using the ESCodec model.
+
+        Args:
+        ----
+            audio (torch.Tensor): Input audio tensor.
+            num_streams (int): Number of streams to use for encoding.
+                Defaults to 6.
+                num_streams*1.5kbps
+
+        Returns:
+        -------
+            tuple: Encoded and decoded audio tensors.
+
+        """
+        codes, f_shape = self.encode(audio, num_streams)
+        decoded_audio = self.decode(codes, f_shape)
+        return codes, decoded_audio
+
+
+if __name__ == "__main__":
+    from src.audio import load_wave, save_wave
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    config_pth = "pretrained/esc9kbps_large_non_adversarial/config.yaml"
+    model_pth = "pretrained/esc9kbps_large_non_adversarial/model.pth"
+    esc = ESCodec(config_pth, model_pth)
+    esc.model.to(device)
+    esc.model.eval()
+    print(esc)
+
+    test_fp = "data/librittsr/14_208_000001_000000.wav"
+    audio, sr = load_wave(test_fp, 16000, mono=True)
+    assert isinstance(audio, torch.Tensor), f"Expected torch.Tensor, got {type(audio)}"
+    audio = audio.unsqueeze(0)
+    audio = audio.to(device)
+    print(audio.shape)
+    codes, f_shape = esc.encode(audio, 6)
+    print(codes.max(), codes.min())
+    rand_int = 50
+    rand_min = 4
+    codes[:, 1:6, :, 100:120] += (
+        torch.randint_like(codes[:, 1:6, :, 100:120], rand_int, device=device, dtype=codes.dtype) - rand_int // 2
+    )
+    codes = torch.clamp(codes, 0, 1021)
+    print(codes.max(), codes.min())
+
+    print(codes.shape, f_shape)
+    decoded_audio = esc.decode(codes, f_shape)
+    print(decoded_audio.shape)
+    save_wave(decoded_audio.squeeze(0).detach().cpu(), "data/librittsr/decoded_n6.wav", 16000)
+
+    """
+    torch.Size([1, 144000])
+    torch.Size([1, 6, 3, 450]) (2, 900)
+    torch.Size([1, 143920])
+    --------------------
+    torch.Size([1, 4, 3, 450]) (2, 900)
+    torch.Size([1, 143920])
+    --------------------
+    torch.Size([1, 2, 3, 450]) (2, 900)
+    torch.Size([1, 143920])
+    --------------------
+    torch.Size([1, 1, 3, 450]) (2, 900)
+    torch.Size([1, 143920])
+    """
