5397d49b3efcdb01b51bbc7f8d2e7d7ccbde9d15
k-washi <bandad.kw@gmail.com>
2025-04-28 15:03:47 +0900

    add clap

---
+++ track1/core/modules/clap.py
@@ -0,0 +1,75 @@
+import torch
+from transformers import ClapModel, ClapProcessor
+
+from src.audio import load_wave
+
+CLAP_SR = 48000
+CPU_DEVICE = torch.device("cpu")
+
+
+class ClapEmbedding:
+    """A class for extracting audio and text features using CLAP (Contrastive Language-Audio Pretraining)."""
+
+    def __init__(self, pretrained_model_name: str = "laion/clap-htsat-fused", device: torch.device = CPU_DEVICE) -> None:
+        """Initialize the CLAP model."""
+        super().__init__()
+        self.device = device
+        self.processor = ClapProcessor.from_pretrained(pretrained_model_name)
+        self.model = ClapModel.from_pretrained(pretrained_model_name).to(device)  # pyright: ignore[reportAttributeAccessIssue]
+
+    def embedding_audio(self, wav_path: str) -> torch.Tensor:
+        """Extract audio features using CLAP.
+
+        Args:
+        ----
+            wav_path (str): Path to the audio file.
+
+        Returns:
+        -------
+            torch.Tensor: Extracted audio features. [512]
+
+        """
+        # Load the audio file
+        wav, _ = load_wave(wav_path, sample_rate=CLAP_SR, mono=True, is_torch=True)
+        assert isinstance(wav, torch.Tensor), f"Audio file {wav_path} is not a torch tensor."
+
+        # Preprocess the audio
+        inputs = self.processor(audios=wav, sampling_rate=CLAP_SR, return_tensors="pt", padding=True).to(self.device)
+
+        # Extract features
+        with torch.no_grad():
+            out = self.model.get_audio_features(**inputs)  # pyright: ignore[reportArgumentType]
+        return out.squeeze(0)
+
+    def embedding_text(self, text: str) -> torch.Tensor:
+        """Extract text features using CLAP.
+
+        Args:
+        ----
+            text (str): Text to be processed.
+
+        Returns:
+        -------
+            torch.Tensor: Extracted text features. [512]
+
+        """
+        # Preprocess the text
+        inputs = self.processor(text=[text], return_tensors="pt", padding=True).to(self.device)
+
+        # Extract features
+        with torch.no_grad():
+            out = self.model.get_text_features(**inputs)  # pyright: ignore[reportArgumentType]
+        return out.squeeze(0)
+
+
+if __name__ == "__main__":
+    # Example usage
+    model = ClapEmbedding()
+    x = model.embedding_audio("src/examples/sample.wav")
+    print(x.shape)  # Should print the shape of the extracted features
+
+    y = model.embedding_text("a man voice.")
+    print(y.shape)  # Should print the shape of the extracted features
+
+    sim = torch.cosine_similarity(x.unsqueeze(0), y.unsqueeze(0))
+    print(sim)  # Should print the cosine similarity between audio and text features
--- track1/doc/DATASET.md
+++ track1/doc/DATASET.md
@@ -3,3 +3,28 @@
 
 gdown https://drive.google.com/uc?id=1KLdpZTgscdMJz0qBUDqnVEKExJzUG5U7 -O ./data/
 ```
+
+`./data/MusicEval-phase1/`にデータを格納することとする。
+
+## データセットを48kHzに拡張
+
+[AudioSR](https://github.com/haoheliu/versatile_audio_super_resolution?tab=readme-ov-file)
+
+```
+uv pip install audiosr
+```
+
+```bash
+ls ./data/MusicEval-phase1/wav/ > ./data/MusicEval-phase1/wav_list.lst
+cd data/MusicEval-phase1/wav
+mkdir ../wav48k_audiosr
+audiosr -il ../wav_list.lst -s ../wav48k_audiosr --model_name basic --ddim_steps 200
+```
+
+# 特徴量の作成
+
+Aux featureの作成
+
+```bash
+wget https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth -P ./pretrained/
+```
